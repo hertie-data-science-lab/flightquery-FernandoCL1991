cb0f8643c08272a83ba9abab948351fab57a568c
Fernando_data_CL <73721626+FernandoCL1991@users.noreply.github.com>
2023-04-10 22:28:54 +0200

    Pushing last changes

--- FlightQuery.py
+++ FlightQuery.py
@@ -1,23 +1,32 @@
 from SortedTableMap import *
-
 class FlightQuery(SortedTableMap):
-    '''An application of SortedTableMap, used to query tickets of expeted period'''
+    '''An application of SortedTableMap, used to query tickets of expected period'''
     class Key:
         __slots__ = "_origin", "_dest", "_date", "_time"
-        pass
+        def __init__(self, origin, dest, date, time):
+            self._origin = origin
+            self._dest = dest
+            self._date = date
+            self._time = time
+
+        def __lt__(self, other):
+            if self._origin < other._origin:
+                return True
+            elif self._origin == other._origin and self._dest < other._dest:
+                return True
+            elif self._origin == other._origin and self._dest == other._dest and self._date < other._date:
+                return True
+            elif self._origin == other._origin and self._dest == other._dest and self._date == other._date and self._time < other._time:
+                return True
+            else:
+                return False
 
     def query(self, k1, k2):
-        pass
-
-
-a = FlightQuery()
-s = [("A", "B", 622, 1200, "No1"), ("A", "B", 622, 1230, "No2"), ("A", "B", 622, 1300, "No3")]
-for each in s:
-    key = a.Key(each[0], each[1], each[2], each[3])
-    value = each[4]
-    a[key] = value
-print(len(a))
-
-k1 = ("A", "B", 622, 1200)
-k2 = ("A", "B", 622, 1300)
-a.query(k1, k2)
+        result = []
+        for key in self._table:
+            if key._origin == k1[0] and key._dest == k1[1]:
+                if k1[2:] <= (key._date, key._time) < k2[2:]:
+                    result.append(key)
+            elif key._origin > k1[0] and key._dest >= k1[1]:
+                break
+        return result
